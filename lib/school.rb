07af86e95a7c1dfcf3fe4b31937c8f58e47860b1
Flatiron School <andrewsprague@outlook.com>
2018-08-29 17:36:58 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+
+    attr_reader :roster, :name
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student (name, grade)
+        if self.has_grade(grade)
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end 
+    end
+
+    def has_grade (grade)
+        @roster.keys.include?(grade)
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade,value|
+            value.sort!
+        end
+    end
+end
